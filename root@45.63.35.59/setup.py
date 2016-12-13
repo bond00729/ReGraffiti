2336ba525a7363f728312a5c5c9b2932485e2f89
Alexis Anand <alexis@alexisanand.com>
2016-12-12 23:37:32 -0800

    Folder name fix

--- root@45.63.35.59/setup.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-
-database = sqlite3.connect('regraffiti.db')
-cursor = database.cursor()
-
-cursor.execute('CREATE TABLE images (location text, date text, image blob)')
-
-database.commit()
-database.close()
